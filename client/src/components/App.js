1ab1969dd7c57fb1c4a9077e7997690baf8e8e03
sunpark <yangheat@gmail.com>
2022-02-15 00:10:19 +0900

    Video upload page init

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LandingPage from "./components/views/LandingPage/LadingPage";
-import LoginPage from "./components/views/LoginPage/LoginPage";
-import RegisterPage from "./components/views/RegisterPage/RegisterPage";
-import VidoeUploadPage from "./components/views/VidoeUploadPage/VidoeUploadPage";
-import Auth from "./hoc/Auth";
+import NavBar from "./views/NavBar/NavBar";
+import LandingPage from "./views/LandingPage/LadingPage";
+import LoginPage from "./views/LoginPage/LoginPage";
+import RegisterPage from "./views/RegisterPage/RegisterPage";
+import VideoUploadPage from "./views/VideoUploadPage/VideoUploadPage";
+import Auth from "../hoc/Auth";
 
 export default function App() {
   const AuthLandingPage = Auth(LandingPage, true);
   const AuthLoginPage = Auth(LoginPage, false);
   const AuthRegisterPage = Auth(RegisterPage, false);
-  const AuthVidoeUploadPage = Auth(VidoeUploadPage, true);
+  const AuthVideoUploadPage = Auth(VideoUploadPage, true);
 
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route exact path="/" element={<AuthLandingPage />} />
         <Route exact path="/login" element={<AuthLoginPage />} />
         <Route exact path="/register" element={<AuthRegisterPage />} />
-        <Route exact path="/vidoeupload" element={<AuthVidoeUploadPage />} />
+        <Route exact path="/video/upload" element={<AuthVideoUploadPage />} />
       </Routes>
     </BrowserRouter>
   );
